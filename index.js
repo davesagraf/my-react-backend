f1450a34cf288cf94f8fb2b2f9d8f0f3aaee015c
dave <d.saguev@leviossa.net>
2022-05-16 19:05:09 +0300

    changed CORS options to connect front end

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const express = require("express");
 require('@prisma/client');
 const app = express();
 require('dotenv').config();
-const session = require("express-session");
 const cors = require('cors')
-const store = new session.MemoryStore();
 const PORT = process.env.PORT || 8000;
 
 const indexRouter = require('./controllers');
 const bodyParser = require('body-parser');
-const auth = require("./middlewares/auth");
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// redirect to routes/index.js
 app.use('/', indexRouter);
-app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
